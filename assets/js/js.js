5493952d8be05b630fd25ec12aeb435e14bb8b62
Isidro Gonzalez <31930997+izidro2928@users.noreply.github.com>
2020-09-13 06:01:51 -0700

    Add files via upload

---
+++ assets/js/js.js
@@ -0,0 +1,10 @@
+
+// var root = document.getElementsByTagName("html")[0];
+// root.setAttribute("class", "is-clipped");
+
+function showModal(){
+    document.getElementById("my-modal").style.display = "flex";
+}
+function hideModal() {
+    document.getElementById("my-modal").style.display = "none";
+}
